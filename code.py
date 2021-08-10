27c53d1a588846c9738135b26de16aecae4b72c6
wineSecret <batinaperhoty@protonmail.com>
2021-08-10 06:50:38 +0300

    Media downloads are now a separate function

--- code.py
+++ code.py
@@ -6,10 +6,13 @@ def generateName(length):
 		name += random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits)
 	return name
 
-while True:
-	name = generateName(5)
+def downloadMedia(length):
+	name = generateName(length)
 	response = requests.get("https://i.imgur.com/" + name + ".jpeg", allow_redirects = False)
 	if response.status_code != 302:
 		with open(name + "." + response.headers["content-type"].split("/")[1], "wb") as f:
 			f.write(response.content)
 			f.close()
+
+while True:
+	downloadMedia(5)
