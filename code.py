fc16b215dd419bcfe8e496b304869a9acf819053
wineSecret <batinaperhoty@protonmail.com>
2021-08-10 06:24:57 +0300

    First version

---
+++ code.py
@@ -0,0 +1,15 @@
+import string, random, requests
+
+def generateName(length):
+	name = ""
+	for i in range(length):
+		name += random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits)
+	return name
+
+while True:
+	name = generateName(5)
+	response = requests.get("https://i.imgur.com/" + name + ".jpeg", allow_redirects = False)
+	if response.status_code != 302:
+		with open(name + "." + response.headers["content-type"].split("/")[1], "wb") as f:
+			f.write(response.content)
+			f.close()
